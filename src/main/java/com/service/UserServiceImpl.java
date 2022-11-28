be42cac47ed3f859cef0e16fff2afafece2302a0
seyha30 <sinseyha1x@gmail.com>
2022-11-29 00:11:36 +0700

    1

--- src/main/java/com/service/UserServiceImpl.java
+++ src/main/java/com/service/UserServiceImpl.java
@@ -1,7 +1,13 @@
 package com.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.domain.Role;
@@ -21,7 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
 	private final UserRepo userRepo;
 	private final RoleRepo roleRepo;
@@ -34,13 +40,13 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public Role saveRole(Role role) {
-		log.info("Saving new role {} to the database" , role.getName());
+		log.info("Saving new role {} to the database", role.getName());
 		return roleRepo.save(role);
 	}
 
 	@Override
 	public void addRoleToUser(String username, String rolename) {
-		log.info("Saving new role {} to the user {}" , rolename,username);
+		log.info("Saving new role {} to the user {}", rolename, username);
 		User user = userRepo.findByUserName(username);
 		Role role = roleRepo.findByName(rolename);
 		user.getRoles().add(role);
@@ -48,16 +54,35 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User getUser(String username) {
-		log.info("Fetching user {}" ,username);
-		
+		log.info("Fetching user {}", username);
+
 		return userRepo.findByUserName(username);
 	}
 
 	@Override
 	public List<User> getUsers() {
 		log.info("Fetching all users");
-	
+
 		return userRepo.findAll();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.findByUserName(username);
+		if (user == null) {
+			log.error("user not found in the database");
+			throw new UsernameNotFoundException("Not found {}" + username);
+		} else {
+			log.info("User found in the database");
+
+		}
+		Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		user.getRoles().forEach(role -> {
+			authorities.add(new SimpleGrantedAuthority(role.getName()));
+		});
+
+		return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(),
+				authorities);
 	}
 
 }
